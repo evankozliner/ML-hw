211f700b499a3dc0a606510e285979c8600bd36d
evankozliner <evankozliner@gmail.com>
2016-04-08 10:32:20 -0400

    Adjusted question 3

--- question_3_actual.m
+++ question_3_actual.m
@@ -4,6 +4,7 @@ k = 300;
 train = load('train79.mat');
 test = load('test79.mat');
 x = train.d79'; 
+x_test = test.d79';
 y = ones(2000, 1); % sevens
 y(1000:2000, 1) = -1; % nines
 
@@ -13,15 +14,9 @@ z = exp(1i * w * x);
 alpha = (eye(k) * lambda + z * z') \ (z * y);
 
 % Test 
-results = alpha' * exp(1i * w * x);
+results = alpha' * exp(1i * w * x_test);
 labels = sign(real(results))';
 numErr = 0;
-loss_7 = sum(labels(1:1000,1) == -1)
-loss_9 = sum(labels(1000:2000,1) == 1)
-for i=1:length(labels)
-    if y(i) ~= labels(i)
-        numErr = numErr + 1;
-    end
-end
-fprintf('Random Fourier Features: %.2f %% error\n', (numErr / 2000) * 100);
-(loss_7/loss_9)/20
+loss_7 = sum(labels(1:1000,1) == -1);
+loss_9 = sum(labels(1000:2000,1) == 1);
+fprintf('RNN gets %.2f %% error\n', (loss_7+loss_9)/20)
